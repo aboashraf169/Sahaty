3ac376cabacc14807ad7f1a4de1a26a1f347853c
mido mj <abo.ashraf169@gmail.com>
2024-12-05 13:58:30 +0200

    Initial Commit

---
+++ Sahaty/SahatyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SahatyApp.swift
+//  Sahaty
+//
+//  Created by mido mj on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SahatyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
